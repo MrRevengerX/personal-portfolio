f8de0b32b7a046e6e44e5c64f90119eb7f0956fb
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-03-08 22:46:57 +0530

    Under constructions page done.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+import coding from "./coding.gif";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container min-h-screen flex flex-col items-center justify-center bg-[#35B993]">
+          <img src={coding} className="w-96" alt="" />
+          <h1 className="font-bold text-[48px] text-white">
+            Under Construction
+          </h1>
+          <h5 className="text-white text-lg">Wanna have a sneak peak?</h5>
+          <a
+            className="text-white flex justify-center items-center gap-2 bg-[#6e5494] px-3 py-2 rounded-lg mt-3 hover:bg-[#5d3c8f] transition-colors duration-300 ease-in-out"
+            href="https://github.com/MrRevengerX/personal-portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub /> GitHub Repository
+          </a>
+        </div>
       </header>
     </div>
   );
